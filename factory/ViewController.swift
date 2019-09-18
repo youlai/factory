7b344cb56731f6ffafc67ebb4b488d1e46c77abc
youlai <761364115@qq.com>
2019-09-18 09:26:53 +0800

    Initial Commit

---
+++ factory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  factory
+//
+//  Created by Apple on 2019/9/18.
+//  Copyright © 2019 zhkj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
